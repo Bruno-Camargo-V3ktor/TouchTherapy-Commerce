89198eed65a88d1ec686b5951a0932d2d476e759
Bruno-Camargo-V3ktor <bruno.ferreira.v3ktor@gmail.com>
2025-12-09 22:46:35 -0300

    refactor: add fields password and auth for User model

--- core/src/models/user.rs
+++ core/src/models/user.rs
@@ -1,4 +1,4 @@
-use super::{Log, Roles};
+use super::{Log, Roles, UserAuth};
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
@@ -6,12 +6,18 @@ use uuid::Uuid;
 #[derive(Serialize, Deserialize)]
 pub struct User<'a> {
     pub id: Uuid,
+
+    pub password: &'a str,
+    pub auth: UserAuth<'a>,
+
     pub full_name: &'a str,
     pub email: &'a str,
     pub cpf: &'a str,
     pub phone: &'a str,
     pub birth: DateTime<Utc>,
+
     pub role: Vec<Roles>,
     pub active: bool,
+
     pub log: Log,
 }
