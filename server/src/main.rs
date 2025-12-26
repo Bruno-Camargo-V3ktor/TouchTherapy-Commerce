a660cd6a7337a4f5ba3435b0972ea5d577a0e90e
Bruno-Camargo-V3ktor <bruno.ferreira.v3ktor@gmail.com>
2025-12-26 15:46:03 -0300

    feat: initilizer server API

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer, Responder, get, web};
+use std::io;
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    HttpServer::new(move || {
+        App::new().service(web::scope("/api").service(web::scope("/users").service(index)))
+    })
+    .bind("0.0.0.0:4321")?
+    .run()
+    .await?;
+
+    Ok(())
+}
+
+#[get("/")]
+async fn index() -> impl Responder {
+    "Hello World"
 }
